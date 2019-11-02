c89e3955436a8068788d150b29ee2ec2f1d09b95
Robert <rjm448@cornell.edu>
2019-11-02 19:38:22 -0400

    skeleton of main implemented

---
+++ dbms/Modules/main.ml
@@ -0,0 +1,26 @@
+open Datardwt
+open Commands
+open Computation
+
+let rec process_queries =
+  print_string "> ";
+  (*Read the command from Terminal *)
+  let comm = read_line () in
+  (*Try to Parse it *)
+  match parse comm with 
+  | exception (Empty) -> process_queries
+  | exception (Malformed) -> 
+    ANSITerminal.(print_string [red] 
+                    "Invalid Command, Please try again.\n");
+    process_queries
+  | msg -> begin
+      match msg with
+      | Quit -> ()
+    end
+
+
+let main () = 
+  ANSITerminal.(print_string [red]
+                  "\n\nWelcome to Ocaml DBMS\n");
+  print_endline "Please enter your query\n";
+  process_queries
