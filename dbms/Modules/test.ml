bb11d31cbce7e8600c3055b1f4249b0a652fc0f3
Daniel Stabile <dis52@cornell.edu>
2019-11-02 21:18:20 -0400

    changed queries to query

--- dbms/Modules/test.ml
+++ dbms/Modules/test.ml
@@ -7,6 +7,25 @@ open Datardwt
 let data_read_write_tests = [
 ]
 
+(* [make_query_test name expected s] constructs an OUnit test named 
+   [name] that asserts the quality of [expected] of [s] applied to 
+   query.parse*)
+let make_query_test name expected s =
+  "query Test: " ^ name >:: (fun _ -> 
+      assert_equal expected (parse s))
+
+(* [malformed_query_test name s] constructs an OUnit test named [name] that 
+   asserts [s] applied to [parse] raises a query.Malformed exception*)
+let malformed_query_test name s =
+  "Malformed query Test: " ^ name >:: (fun _ -> 
+      assert_raises Malformed (fun () ->  (parse s)))
+
+(* [malformed_query_test name s] constructs an OUnit test named [name] that 
+   asserts [s] applied to query.parse raises a query.Empty exception*)
+let empty_query_test name s =
+  "Empty query Test: " ^ name >:: (fun _ -> 
+      assert_raises Empty (fun () ->  (parse s)))
+
 let queries_tests = [
 
 ]
@@ -14,8 +33,6 @@ let queries_tests = [
 let computatoin_tests = [
 
 ]
-
-
 
 let suite =
   "test suite for dbms"  >::: List.flatten [
