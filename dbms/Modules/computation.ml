0f8fb2d98368136146fb352109b644c636bc97f1
Vivian Li <vml39@cornell.edu>
2019-11-02 20:11:05 -0400

    begin computation.ml

---
+++ dbms/Modules/computation.ml
@@ -0,0 +1,24 @@
+let where param = 
+  failwith "unimplemented"
+
+let like param = 
+  failwith "unimplemented"
+
+let order param = 
+  failwith "unimplemented"
+
+let sort param = 
+  failwith "unimplemented"
+
+(* qry = object phrase *)
+let select qry =
+  failwith "unimplemented"
+
+let insert qry = 
+  failwith "unimplemented"
+
+let delete qry = 
+  failwith "unimplemented"
+
+let join qry = 
+  failwith "unimplemented"
--- dbms/Modules/queries.ml
+++ dbms/Modules/queries.ml
@@ -9,7 +9,7 @@
 
 type object_phrase = string list
 
-type command = 
+type query = 
   | Scan of object_phrase
   | Select of object_phrase
   (* SELECT * FROM table *)
@@ -17,10 +17,6 @@ type command =
   (* INSERT INTO table_name (column1, column2, column3, ...)
      VALUES (value1, value2, value3, ...); *)
   | Delete of object_phrase
-  | Where of object_phrase
-  | Like of object_phrase
-  | Order of object_phrase
-  | Sort of object_phrase
   | Join of object_phrase
   | Quit
 
@@ -41,10 +37,6 @@ let parse str =
   | h::t when h = "SELECT" -> if t = [] then raise Malformed else Select t
   | h::t when h = "INSERT" -> if t = [] then raise Malformed else Insert t
   | h::t when h = "DELETE" -> if t = [] then raise Malformed else Delete t
-  | h::t when h = "WHERE" -> if t <> [] then raise Malformed else Where t
-  | h::t when h = "LIKE" -> if t <> [] then raise Malformed else Like t
-  | h::t when h = "ORDER" -> if t <> [] then raise Malformed else Order t
-  | h::t when h = "SORT" -> if t <> [] then raise Malformed else Sort t
   | h::t when h = "JOIN" -> if t <> [] then raise Malformed else Join t
   | h::t when h = "quit" -> if t <> [] then raise Malformed else Quit
   | _ -> raise Malformed
