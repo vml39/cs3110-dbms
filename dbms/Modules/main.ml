56d2e47c7c912afdc621e6f4a9b92da1dc5c9240
Robert <rjm448@cornell.edu>
2019-11-02 20:57:20 -0400

    fixed scan error in main

--- dbms/Modules/main.ml
+++ dbms/Modules/main.ml
@@ -2,22 +2,21 @@ open Datardwt
 open Queries
 open Computation
 
-let rec process_queries =
+let rec process_queries () =
   print_string "> ";
   (*Read the command from Terminal *)
   let comm = read_line () in
   (*Try to Parse it *)
   match parse comm with 
-  | exception (Empty) -> process_queries
+  | exception (Empty) -> process_queries ()
   | exception (Malformed) -> 
     ANSITerminal.(print_string [red] 
                     "Invalid Command, Please try again.\n");
-    process_queries
+    process_queries ()
   | msg -> begin
       match msg with
       | Quit -> print_endline "Goodbye for now.\n";
         exit 0
-      | Scan obj -> ()
       | Select obj -> ()
     end
 
@@ -26,4 +25,4 @@ let main () =
   ANSITerminal.(print_string [red]
                   "\n\nWelcome to Ocaml DBMS\n");
   print_endline "Please enter your query\n";
-  process_queries
+  process_queries ()
