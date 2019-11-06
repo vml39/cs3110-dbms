3d2a63b21f1602cdd7a80cc77096f8b0a336d1c6
Vivian Li <vml39@cornell.edu>
2019-11-06 11:31:49 -0500

    delete hardcode schema

--- dbms/Modules/computation.ml
+++ dbms/Modules/computation.ml
@@ -94,13 +94,8 @@ let rec filter_table schema acc = function
       List.filter (fun _ -> i := !i + 1; List.nth schema !i) h in
     filter_table schema (filtered_row::acc) t
 
-(* schema from txt is failing *)
-let test_schema = 
-  [("students", ["name"; "netid"; "gradyear"; "major"; "home"])]
-
 let select qry =
-  (* let schema = table_schema (schema_from_txt ()) (select_table qry) in  *)
-  let schema = table_schema test_schema (select_table qry) in
+  let schema = table_schema (schema_from_txt ()) (select_table qry) in 
   let fields = select_fields [] qry |> filter_fields schema [] in 
   let table = 
     table_from_txt (select_table qry) |> filter_table fields [] in
