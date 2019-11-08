52af274d6632d245eef6e4ba5b0ba8dfe676f865
Robert <rjm448@cornell.edu>
2019-11-07 19:41:06 -0500

    updates for propper submission

---
+++ dbms/Modules/authors.mli
@@ -0,0 +1,11 @@
+(**
+   Metadata about this submission.
+   @author TODO All author names and NetIDs.
+   Robert Morgowicz rjm448
+   Daniel Stabile dis52 
+   Vivian Li (vml39)
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
