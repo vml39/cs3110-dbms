6169e85f1a8e174d5ecf35a0ce8bd88f5856d1a2
Robert <rjm448@cornell.edu>
2019-12-07 22:13:50 -0500

    added more help strings

--- dbms/Modules/computation.ml
+++ dbms/Modules/computation.ml
@@ -379,82 +379,85 @@ let drop_table qry =
   Sys.remove (get_path qry.table)
 
 let general_msg = 
-  "'HELP' Command \n"
+  "'HELP' Command: \n"
   ^ "To get help for a command, type HELP and the command name \n"
   ^ "The list of valid commands is: SELECT, INSERT INTO, DELETE, TRUNCATE"
   ^ " TABLE, CREATE TABLE, DROP TABLE, CHANGE DATABASE, READ, HELP, and QUIT\n"
 
 let select_msg = 
-  "'SELECT' QUERY \n"
+  "'SELECT' QUERY: \n"
 
 let insert_msg = 
-  "'INSERT INTO' QUERY \n"
-  ^ "INSERT INTO inserts a new row into a table inserting data into the"
+  "'INSERT INTO' QUERY: \n"
+  ^ "'INSERT INTO' inserts a new row into a table inserting data into the"
   ^ " specified coulmns\n"
   ^ "USAGE: 1) INSERT INTO [tablename] (f1, f2,... fn) VALUES (v1, v2,... vn)\n"
   ^ "       2) INSERT INTO [tablename] VALUES (v1, v2,... vn)*\n"
-  ^ "Requires: [tablename] is the name of an existing table and" 
+  ^ "REQUIRES: [tablename] is the name of an existing table and" 
   ^ "(f1, f2,... fn) are colmum names in [tablename]\n"
   ^ "*For this case, length of (v1, v2,... vn) must match the number of"
   ^ " columns\n"
 
 let delete_msg = 
-  "'DELETE' QUERY \n"
-  ^ "DELETE deletes rows from a table, optionally meeting a given condition\n"
+  "'DELETE' QUERY: \n"
+  ^ "'DELETE' deletes rows from a table, optionally meeting a given condition\n"
   ^ "USAGE: 1) DELETE FROM [tablename]\n"
   ^ "       2) DELETE FROM [tablename] WHERE [b]\n"
-  ^ "Requires: [tablename] is the name of an existing table and"
+  ^ "REQUIRES: [tablename] is the name of an existing table and"
   ^ "b is a conditional following WHERE guidelines\n"
 
 let truncate_msg = 
-  "'TRUNCATE TABLE' QUERY \n"
-  ^ "TRUNCATE TABLE removes all data from a specified table\n"
+  "'TRUNCATE TABLE' QUERY: \n"
+  ^ "'TRUNCATE TABLE' removes all data from a specified table\n"
   ^ "USAGE: TRUNCATE TABLE [tablename]\n"
-  ^ "Requires: [tablename] is the name of an existing table\n"
+  ^ "REQUIRES: [tablename] is the name of an existing table\n"
+
 
 let create_msg = 
-  "'CREATE TABLE' QUERY \n"
-  ^ "CREATE TABLE creates a new, empty table with the specified name\n"
+  "'CREATE TABLE' QUERY: \n"
+  ^ "'CREATE TABLE' creates a new, empty table with the specified name\n"
   ^ "USAGE: CREATE TABLE [tablename]\n"
-  ^ "Requires: [tablename] is a valid table name (no whitespace, commas,)\n"
+  ^ "REQUIRES: [tablename] is a valid table name (no whitespace, commas, \n"
+  ^ "parentheses, colons, or other syntactiaclly improtant characters)"
+  ^ "oh? Create!\n"
 
 let drop_msg = 
-  "'DROP TABLE' QUERY \n"
-  ^ "DROP TABLE removes a specified table from the database\n"
+  "'DROP TABLE' QUERY: \n"
+  ^ "'DROP TABLE' removes a specified table from the database\n"
   ^ "USAGE: DROP TABLE [tablename]\n"
-  ^ "Requires: [tablename] is the name of an existing table\n"
+  ^ "REQUIRES: [tablename] is the name of an existing table\n"
 
 let change_msg = 
-  "'CHANGE DATABASE' Command \n"
-  ^ "CHANGE DATABASE changes the working database\n"
+  "'CHANGE DATABASE' Command: \n"
+  ^ "'CHANGE DATABASE' changes the working database\n"
   ^ "USAGE: CHANGE DATABASE [databasename]\n"
-  ^ "Requires: [databasename] is the name of an existing database visible to"
+  ^ "REQUIRES: [databasename] is the name of an existing database visible to"
   ^ " the system\n"
 
 let read_msg = 
-  "'READ' Command \n"
-  ^ "READ reads queries from an input file and sends their results to an output"
-  ^ "file\n"
+  "'READ' Command: \n"
+  ^ "'READ' reads queries from an input file and sends their results to an"
+  ^ " output file\n"
   ^ "USAGE: READ FROM [filename]\n"
-  ^ "Requires: [filename] is the name of an existing file in the 'input' folder"
+  ^ "REQUIRES: [filename] is the name of an existing file in the 'input' folder"
   ^ "containing properly formatted queries\n"
 
 let quit_msg = 
-  "'QUIT' Command \n"
-  ^ "QUIT exits the dbms\n"
+  "'QUIT' Command: \n"
+  ^ "'QUIT' exits the dbms\n"
   ^ "USAGE: QUIT\n"
 
 let help_msg = 
-  "'HELP' Command \n"
+  "'HELP' Command: \n"
 
 let where_msg = 
-  "'WHERE' Keyword \n"
+  "'WHERE' Keyword: \n"
 
 let join_msg = 
-  "'JOIN' Keyword \n"
+  "'JOIN' Keyword: \n"
 
 let order_msg = 
-  "'ORDER BY' Keyword \n"
+  "'ORDER BY' Keyword: \n"
 
 let help_with qry =
   match qry.s1, qry.s2 with
@@ -472,4 +475,4 @@ let help_with qry =
   | "WHERE", _ -> where_msg
   | "JOIN", _ -> join_msg
   | "ORDER", "BY" -> order_msg
-  | _ -> "Sorry, I can't help you with that\n"
+  | s1, s2 -> "Sorry, I can't help you with '" ^ s1 ^ " " ^ s2 ^ "'\n"
